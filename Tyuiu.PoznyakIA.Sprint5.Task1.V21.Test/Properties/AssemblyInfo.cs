aa5b2a0db33d8c0c945d66b268cd2ef1d27db755
Tyuiu.PoznyakIA@outlook.com <igor.poznyak.2005@mail.ru>
2023-11-19 19:43:16 +0500

    Task1

---
+++ Tyuiu.PoznyakIA.Sprint5.Task1.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoznyakIA.Sprint5.Task1.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoznyakIA.Sprint5.Task1.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da6a6eea-617c-4354-83f8-ea435e0e277c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
